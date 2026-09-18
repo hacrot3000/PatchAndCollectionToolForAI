fd09d6d26864376b32f4e1e560ebb41fbbde6e51
Trương Chương Dương <a0a1a2@gmail.com>
2026-09-18 10:19:42 +0700

    Load broadcast groups before tab context menu

--- vscode_tasks_menu_go/web/featuremods/next.js
+++ vscode_tasks_menu_go/web/featuremods/next.js
@@ -17,5 +17,6 @@ import '/featuremods/ptvpriority.js';
 import '/featuremods/clearfiles.js';
 import '/featuremods/selfupdate.js';
 import '/featuremods/menus.js';
+import '/featuremods/broadcast.js';
 import '/featuremods/tabcontext.js';
 import '/featuremods/viewportfix.js';
